f94e750d407a2a1236167d866bf50ebf96ab8d61
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-02-03 23:25:01 +0100

    Bench 3198964

--- src/search.cpp
+++ src/search.cpp
@@ -827,7 +827,7 @@ Value Search::Worker::search(
     // Step 9. Null move search with verification search
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
         && ss->staticEval >= beta - 20 * depth + 470 - 60 * improving && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
+        && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
     {
         assert(eval - beta >= 0);
 
