c930c22dea270b4bf9a20002cf54b64fc5541171
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-11-22 11:41:47 +0100

    Bench 2980489

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
-            r += 1119;
+            r += 1119 - (1000 * (move == ttData.move));
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 2)
